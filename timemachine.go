1d248aecb7c10e10f3367c9346e3e7af075e5677
rmatsuoka <76464810+rmatsuoka@users.noreply.github.com>
2023-10-10 18:49:36 +0900

    init

---
+++ timemachine.go
@@ -0,0 +1,36 @@
+package timemachine
+
+import (
+	"context"
+	"time"
+)
+
+// TimeMachine は現在の時間を返す now を実装している。nowFunc が nil のとき now はローカルの現在の時間を返す。
+// non-nil では now は nowFunc 呼び出す。つまり nowFunc が返す値によって現在の時間を操作することができる。
+// context に入れると良さそう。
+type TimeMachine struct {
+	NowFunc func() time.Time
+}
+
+func (tm TimeMachine) Now() time.Time {
+	if tm.NowFunc == nil {
+		return time.Now()
+	}
+	return tm.NowFunc()
+}
+
+type key int
+
+var timeMachineKey key
+
+func ContextWithTimeMachine(ctx context.Context, tm TimeMachine) context.Context {
+	return context.WithValue(ctx, timeMachineKey, tm)
+}
+
+func TimeMachineFromContext(ctx context.Context) TimeMachine {
+	tm, ok := ctx.Value(timeMachineKey).(TimeMachine)
+	if !ok {
+		return TimeMachine{}
+	}
+	return tm
+}
